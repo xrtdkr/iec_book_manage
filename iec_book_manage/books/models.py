9197d3fa9e675ff47402f22cb9984c73dbddd3ca
LumiaXu <xr19960722@gmail.com>
2015-09-14 20:17:07 +0800

    the_forth_push
    beside digmingfei

--- iec_book_manage/books/models.py
+++ iec_book_manage/books/models.py
@@ -14,7 +14,7 @@ class Book(models.Model):
     bookName = models.CharField(max_length=20)
     bookNum = models.CharField(max_length=20)
     bookTime = models.DateTimeField(default=None)
-    bookFlag = models.BooleanField(default=True)    #布尔值True表示可借,布尔值为False不可借python
+    bookFlag = models.BooleanField(default=True)    # 布尔值True表示可借,布尔值为False不可借python
     bookPerson = models.ForeignKey(Person, null=True)
 
     def __unicode__(self):
--- iec_book_manage/books/views.py
+++ iec_book_manage/books/views.py
@@ -22,24 +22,46 @@ def secret(request):
             book_num = request.POST['book_num']
             book_name = request.POST['book_name']
             book_list_lite = Book.objects.all()     # book_list_lite:这个query_Set为在book_list正式参与渲染模板之前所有的所有book对象的表
+            copy_flag = False
             for book_lite in book_list_lite:        # book_list:为参与渲染之前,现有的表中的对象
-                book_lite
-
-            '''now = datetime.datetime.now()
-            book = Book(bookName=book_name)
-            book.bookNum = book_num
-            book.bookFlag = True
-            book.bookTime = now'''
-
-            book.save()
+                if book_num == book_lite.bookNum or book_name == book_lite.bookName:
+                    copy_flag = True
+                break
+            if not copy_flag:
+                now = datetime.datetime.now()
+                book = Book(bookName=book_name)
+                book.bookNum = book_num
+                book.bookFlag = True
+                book.bookTime = now
+                book.save()
+            else:
+                return HttpResponse('You commit a copy ^~^')  # 不可以简单粗暴地直接返回http——response改进:1变成重新渲染模板一下的网页提示2使用ajax进行交互
             book_list = Book.objects.all()
             return render_to_response('seccret.html', {'book_set': book_list}, context_instance=RequestContext(request))
         else:
             return HttpResponse('Sorry, you submit no book HaHa')  # 这里设计的缺少合理之处:改进:1.变成重新渲染模板一下的网页提示2.使用ajax进行交互
-
-    else:
+    elif 'book_delete' in request.POST:
+        if request.POST['book_delete']:    # 对请求的书本删除进行回应
+            book_delete = request.POST['book_delete']
+            book_list_lite = Book.objects.all()    # 同样的这个是临时的那个book表单
+            # 对于无此匹配表单的回应:
+            delete_flag = False
+            for book_lite in book_list_lite:
+                if book_delete == book_lite.bookNum:
+                    Book.objects.filter(bookNum=book_delete).delete()
+                    delete_flag = True
+                    book_list = Book.objects.all()
+                    return render_to_response('seccret.html', {'book_set': book_list},
+                                              context_instance=RequestContext(request))
+                break    # 有匹配表单
+            if not delete_flag:
+                return HttpResponse('sorry,no book in database can match the book that you what to delete')
+        else:    # 对于空表单的回应
+            return HttpResponse('你上传的是空表单,刷新一下页面回去重新填写哦')
+    else:  # 这个else是用来回应直接请求网页的时候,给出渲染
         book_list = Book.objects.all()
         return render_to_response('seccret.html', {'book_set': book_list}, context_instance=RequestContext(request))
+    # secret 部分初步完成
 
 
 def borrow(request):
